e3be2b32a935e81cd7eef356a04e4a7be59d85de
Kyeonghwan Son <snapperbay4453@gmail.com>
2023-08-22 03:24:05 +0900

    Install Spring Boot

---
+++ springboot/src/test/kotlin/com/example/keyclocknextjsspringbootexample/KeyclockNextjsSpringbootExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.keyclocknextjsspringbootexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeyclockNextjsSpringbootExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
